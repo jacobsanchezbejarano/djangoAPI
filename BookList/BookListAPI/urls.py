ce360fbb4db0f469a3705524bc2f1e4599598a5f
jacobsanchezbejarano <jake_76847@hotmail.com>
2024-05-31 11:49:00 -0400

    APIViews

--- BookList/BookListAPI/urls.py
+++ BookList/BookListAPI/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('books/', views.books),
+    # path('books/', views.books),
+    path('books/', views.BookList.as_view()),
+    path('books/<int:pk>', views.Book.as_view()),
 ]
--- BookList/BookListAPI/views.py
+++ BookList/BookListAPI/views.py
@@ -2,8 +2,28 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
 
 @api_view(['GET'])
 def books(request):
-    return Response('List of books', status=status.HTTP_200_OK)
+    return Response('List of books', status.HTTP_200_OK)
+
+
+class BookList(APIView):
+    def get(self, request):
+        author = request.GET.get('author')
+        if author:
+            return Response({"message": "List of the books by " + author}, status.HTTP_200_OK)
+        return Response({"message": "List of the books"}, status.HTTP_200_OK)
+
+    def post(self, request):
+        return Response({"title": request.data.get('title')}, status.HTTP_201_CREATED)
+
+
+class Book(APIView):
+    def get(self, request, pk):
+        return Response({"message": "Single book with id: " + str(pk)}, status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        return Response({"title": request.data.get('title')}, status.HTTP_200_OK)
